6f208a19b4bb4b2e1d5361830e5c958a393c75f9
vfreis <vinicios.falqueiro@gmail.com>
2023-03-21 20:33:27 -0300

    alteração na estrutura de pastas do backend flask

--- backend/app.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.sql import func
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__name__':
-    app.run(debug = True)
--- backend/app/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from .models import Usuario
-from . import db
-
-views = Blueprint('views', __name__)
-
-@views.route('/', methods = ['GET'])
-def home():
-    return render_template('index.html')
