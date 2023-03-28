4db0db54880ecd8a38e11b5535269326b2b1f005
vfreis <vinicios.falqueiro@gmail.com>
2023-03-28 14:32:10 -0300

    teste json adicionado para setup de rota flask-react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,30 @@ import RoutesApp from "./routes";
 import { AuthProvider } from "./contexts/auth";
 import GlobalStyle from "./styles/global";
 
+// const axios = require('axios')
+
+// const testeJson = {
+//     "nome": "uil2",
+//     "data_nascimento" : "17071991",
+//     "sexo" : "sexo",
+//     "email" : "email",
+//     "telefone" : "telefone",
+//     "senha" : "123456789"
+// }
+
+// async function makeRequest(testeJson) {
+
+//     const config = {
+//         method: 'post',
+//         url: 'http://127.0.0.1:5000/inserircadastro'
+//     }
+
+//     let res = await axios(config)
+
+//     console.log(res.status);
+// }
+
+// makeRequest(testeJson);
 
 
 const App = () => (
