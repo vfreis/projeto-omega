2d853e88af5b4f9132a98fc247274ddba7091d0b
vfreis <vinicios.falqueiro@gmail.com>
2023-05-30 17:39:30 -0300

    docker file

--- frontend/src/pages/Footer/index.js
+++ frontend/src/pages/Footer/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faGoogle, faInstagram, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faFacebookF, faTwitter, faGoogle, faInstagram, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
@@ -8,17 +8,17 @@ function Footer() {
       <div className="container ">
         <section className="mb-4">
           <a className="btn btn-primary btn-floating m-1 "  href="#!" role="button">
-            <FontAwesomeIcon icon={faFacebookF} />
+            {/* <FontAwesomeIcon icon={faFacebookF} /> */}
           </a>
           
           <a className="btn btn-primary btn-floating m-1" style={{backgroundColor: "#ac2bac"}} href="#!" role="button">
-            <FontAwesomeIcon icon={faInstagram} />
+            {/* <FontAwesomeIcon icon={faInstagram} /> */}
           </a>
           <a className="btn btn-primary btn-floating m-1" style={{backgroundColor: "#0082ca"}} href="#!" role="button">
-            <FontAwesomeIcon icon={faLinkedinIn} />
+            {/* <FontAwesomeIcon icon={faLinkedinIn} /> */}
           </a>
           <a className="btn btn-primary btn-floating m-1" style={{backgroundColor: "#333333"}} href="#!" role="button">
-            <FontAwesomeIcon icon={faGithub} />
+            {/* <FontAwesomeIcon icon={faGithub} /> */}
           </a>
         </section>
         <section>
